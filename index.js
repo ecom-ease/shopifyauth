80da9337ef01ccaccffb1bc21f1289bad3b41992
ecom-ease <contact@ecomease.com.au>
2025-05-24 22:25:05 +1000

    index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT;
 
 const {
   SHOPIFY_API_KEY,
